7b73d7ab9b5eddc608f7f7b5f9f6d93044c3bb6c
Kiril <csgocvadimkiril80@gmail.com>
2026-01-21 22:33:27 +0100

    chore: add helper script fix.sh for environment fixes

---
+++ bash/fix.sh
@@ -0,0 +1,20 @@
+
+cat > .venv/bin/sph << 'EOF'
+#!/usr/bin/env python3
+import sys
+import os
+
+# Добавляем путь к проекту
+sys.path.insert(0, '/home/kiril/Sphere')
+
+from sphere.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app())
+EOF
+
+chmod +x .venv/bin/sph
+
+echo "Shebang fixed in .venv/bin/sph"
+echo "sph Version:"
+sph -v
