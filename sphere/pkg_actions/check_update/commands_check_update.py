e82a49cf5aeb31a3b70b7dd49dd5f7606b10d748
Kiril <csgocvadimkiril80@gmail.com>
2026-01-27 20:59:43 +0100

    connect check-update

--- sphere/pkg_actions/check_update/commands_check_update.py
+++ sphere/pkg_actions/check_update/commands_check_update.py
@@ -4,9 +4,16 @@ import typer
 
 from .check_update import check_update
 
-def commands_install(app: typer.Typer):
+def commands_check_update(app: typer.Typer):
     
-    @app.command("install", help="Install packages ")
-    def install(package: str):
-        install(package)
+    @app.command(
+        "check-update",
+        help=(
+            "Check for available system package updates.\n"
+            "Supports multiple package managers (dnf, apt, pacman, zypper).\n"
+            "Displays a list of packages that can be updated without actually performing the update.\n"
+        )
+    )
+    def update_check():
+        check_update()
         raise typer.Exit()
--- sphere/pkg_actions/register_pkg_command.py
+++ sphere/pkg_actions/register_pkg_command.py
@@ -1,8 +1,9 @@
 
 from .update.commands_update import commands_update
 from .install.commands_install import commands_install
-
+from .check_update.commands_check_update import commands_check_update
 
 def register_pkg_commands(app):
     commands_update(app)
     commands_install(app)
+    commands_check_update(app)
